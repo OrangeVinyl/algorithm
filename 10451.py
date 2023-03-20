03d876eb49c310dd31309374e9d1ecde1711ca6b
OrangeVinyl <hap_hazard@naver.com>
2023-03-21 00:51:24 +0900

    [ADD] 이코테_DP_예제_개미전사

---
+++ 10451.py
@@ -0,0 +1,8 @@
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    per = []
+    for i in range(n):
+        per.append(map(int,input()))
+    print(per)
---
+++ 이것이 코딩테스트/DP/개미전사.py
@@ -0,0 +1,15 @@
+#이코테 DP_예제_개미전사_문제풀이
+n = int(input())
+array = list(map(int,input().split()))
+dp = [0] * (100)
+
+dp[0] = array[0]
+dp[1] = max(array[0],array[1])
+
+for i in range(2,n):
+    dp[i] = max(dp[i-1], dp[i-2]+array[i])
+    
+print(dp[n-1])
+
+
+
