0604b26af77d2eef37a6b29a21d931728366fad2
Suhwan Choi <9ou5oo@gmail.com>
2024-02-21 18:36:25 +0900

    [Silver V] Title: 영화감독 숌, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int n, cnt;
+int s = 666;
+
+int main() {
+  cin >> n;
+
+  while(1) {
+    if(to_string(s).find("666") != string::npos) {
+      cnt++;
+    }
+
+    if (cnt == n) {
+      cout << s;
+      break;
+    }
+
+    s++;
+  }
+
+  return 0;
+}
