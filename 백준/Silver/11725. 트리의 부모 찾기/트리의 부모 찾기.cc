8e3bf634a9e5250d54b9928ffc50253017b702f7
Suhwan Choi <9ou5oo@gmail.com>
2024-08-09 23:34:00 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 100 ms, Memory: 12448 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -4,27 +4,27 @@
 using namespace std;
 #define MAX 100001
 
-int N;
-int arr[MAX];
+int n;
+int res[MAX];
 bool visited[MAX];
 vector<int> v[MAX];
 
 void dfs(int k) {
-    visited[k]=true;
-    for(int i=0;i<v[k].size();i++) {
+    visited[k] = true;
+    for (int i = 0; i < v[k].size(); i++) {
         int next = v[k][i];
-        if(!visited[next]) {
-            arr[next]=k;
+        if (!visited[next]) {
+            res[next] = k;
             dfs(next);
         }
     }
 }
 
 int main() {
-    cin >> N;
+    cin >> n;
 
-    for(int i=0;i<N;i++) {
-        int x,y;
+    for (int i = 0; i < n; i++) {
+        int x, y;
         cin >> x >> y;
         v[x].push_back(y);
         v[y].push_back(x);
@@ -32,5 +32,7 @@ int main() {
 
     dfs(1);
 
-    for(int i=2;i<=N;i++) cout << arr[i] << "\n";
+    for (int i = 2; i <= n; i++) cout << res[i] << '\n';
+
+    return 0;
 }
